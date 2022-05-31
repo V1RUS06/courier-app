abe9b5119fcd65de3c2944687e9d24c0ab3f8b85
Denis <denison4@mail.ru>
2022-05-31 18:21:33 +0300

    Сделал компонент кнопки

---
+++ src/components/Button/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+export const Button = ({onPress, name}) => {
+  return (
+    <TouchableOpacity style={styles.btn} onPress={onPress}>
+      <Text style={styles.btnText}>{name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    backgroundColor: '#E8F9FA',
+    borderRadius: 20,
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+  },
+  btnText: {
+    color: '#4B595C',
+    fontSize: 13,
+    marginLeft: 10,
+  },
+});
